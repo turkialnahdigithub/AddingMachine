1d92f6266a8994ec7eb22683bac0d8b795a60e34
Turki <turkialnahdigithub@gmail.com>
2019-10-10 00:05:03 -0700

    New changes

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -4,17 +4,18 @@
  *	Assignment 2: This program adds and subtract input number and display the whole process 
  */
 package cse360assign2;
-
+import java.util.*;
 public class AddingMachine {
 
 	//declare private var named total 
 	private int total;
-	
+	private ArrayList<String> list;
 	/**
 	 * Class constructor
 	 */
 	public AddingMachine() {
 		total = 0;
+		list.add("0 ");
 	}
 	
 	/**
@@ -22,7 +23,7 @@ public class AddingMachine {
 	 * @return total 
 	 */
 	public int getTotal() {
-		return 0;
+		return total;
 	}
 	
 	/**
@@ -30,6 +31,8 @@ public class AddingMachine {
 	 * @param value
 	 */
 	public void add (int value) {
+		total += value;
+		list.add("+ " + value + " ");
 		
 	}
 	/**
@@ -37,14 +40,20 @@ public class AddingMachine {
 	 * @param value
 	 */
 	public void subtract(int value) {
-		
+		total -= value;
+		list.add("- " + value + " ");
 	}
 	
 	/**
 	 * toString method 
 	 */
 	public String toString() {
-		return "";
+		String str = "";
+		for(int i = 0; i < list.size(); i++)
+		{
+			str += list.get(i);
+		}
+		return str;
 	}
 	/**
 	 * clear method
